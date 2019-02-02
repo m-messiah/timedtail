278b8f288872ee62524675f3888f2e82adeb1e8e
Maxim Muzafarov <m-messiah@yandex-team.ru>
2019-02-02 14:54:08 +0300

    Use -n seconds, count left and right time borders

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 		os.Exit(1)
 	}
 	timeNow := time.Now()
-	deltaTime := flag.Duration("n", time.Duration(300)*time.Second, "Delta time count from now() to look at from the end of the <log file>")
+	deltaSeconds := flag.Int64("n", 300, "Delta time count from now() to look at from the end of the <log file>")
 	fromTime := flag.Int64("b", 0, "From which datetime start to operate seconds of the <log file>")
 	timestampType := flag.String("t", "common", "Timestamp type")
 	timestampCustomRegex := flag.String("r", "", "Regexp to pick timestamp from string ($1 must select timestamp)")
@@ -31,7 +31,7 @@ func main() {
 	if len(log_files) == 0 {
 		flag.Usage()
 	}
-	assertFlagIsPositiveInt(deltaTime.Nanoseconds())
+	assertFlagIsPositiveInt(*deltaSeconds)
 	assertFlagIsPositiveInt(*fromTime)
 	assertFlagIsPositiveInt(*junkLines)
 	if *timestampType != "common" && *timestampCustomRegex != "" {
@@ -54,17 +54,26 @@ func main() {
 		}
 	}
 	timestampRegex := regexp.MustCompile(timestampRegexString)
+	var timeRightBorder time.Time
+	if *fromTime != 0 {
+		timeRightBorder = time.Unix(*fromTime, 0)
+	} else {
+		timeRightBorder = timeNow
+	}
+	timeLeftBorder := timeRightBorder.Add(-time.Duration(*deltaSeconds) * time.Second)
 
 	for _, log_file := range log_files {
 		fmt.Print(log_file, ",")
 	}
 	fmt.Println("")
 	fmt.Println(timeNow)
-	fmt.Println(*deltaTime)
+	fmt.Println(*deltaSeconds)
 	fmt.Println(*fromTime)
 	fmt.Println(*timestampType)
 	fmt.Println(*timestampCustomRegex)
 	fmt.Println(*junkLines)
 	fmt.Println(timestampRegex)
+	fmt.Println(timeRightBorder)
+	fmt.Println(timeLeftBorder)
 
 }
